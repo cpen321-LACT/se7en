0215697af933a0208acda6d0c5c33aafe1aed060
Adam Dixon <adamdixon@server.ca>
2019-10-21 17:54:42 -0700

    Added settings for push notifications - i think

--- android/app/src/main/java/com/se7en/MainApplication.java
+++ android/app/src/main/java/com/se7en/MainApplication.java
@@ -4,11 +4,15 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;                       
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -38,6 +42,17 @@ public class MainApplication extends Application implements ReactApplication {
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
+
+@Override
+protected List<ReactPackage> getPackages() {
+  return Arrays.<ReactPackage>asList(
+ new MainReactPackage(),
+            new RNFirebasePackage(),
+ new RNFirebasePackage(),
+ new RNFirebaseMessagingPackage(),
+ new RNFirebaseNotificationsPackage()
+ );                               
+}
 
   @Override
   public void onCreate() {
