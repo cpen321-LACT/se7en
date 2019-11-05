f4719a43bfbb82a99119eaa69d3dd8824882cd7e
Christina Sun <regurgamz@gmail.com>
2019-11-05 00:08:44 -0800

    codacy

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -3,7 +3,7 @@
  */
 
 import "react-native";
-import React from "react";
+//import React from "react"; uncomment when we test with jsx object
 import App from "../App";
 
 // Note: test renderer must be required after react-native.
