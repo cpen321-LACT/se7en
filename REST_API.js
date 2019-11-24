cfb460fe2b1fd3e19441a4af4b74f3c0c9a6e3d0
khatung <tungdjack@gmail.com>
2019-11-23 23:23:25 -0800

    fix potential?

--- REST_API.js
+++ REST_API.js
@@ -585,7 +585,7 @@ app.get("/user/:userId/matches/potentialMatches/:eventId", async (req,res) => {
 
     scheduleDb.collection("scheduleClt").find(timeDateQuery).toArray((err, userScheduleEvent) => {
 
-      if (userScheduleEvent[0] === null){
+      if (doesntExist(userScheduleEvent[0])){
         res.send("There are no users in the database\n");
         return;
       }
